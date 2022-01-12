c1b57a73724b40dc09245b82df0051b93e51fd43
Simone Ballard <simone.a.ballard@gmail.com>
2022-01-12 15:04:34 -0700

    added some main page nesting etc

---
+++ moonshot/src/components/Main.js
@@ -0,0 +1,13 @@
+// some kind of stylized landing(home) page here
+import React from 'react'
+import Pics from './components/Pics'
+
+const Main = () => {
+    return (
+        <div>
+            <Pics />
+        </div>
+    )
+}
+
+export default Main
