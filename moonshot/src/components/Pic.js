aa043c973dff53e8f513dba9bb511cf44465b89f
Simone Ballard <simone.a.ballard@gmail.com>
2022-01-13 17:17:37 -0700

    card component appears to be altering somewhat

--- moonshot/src/components/Pic.js
+++ moonshot/src/components/Pic.js
@@ -1,23 +1,30 @@
 //this component will be like individual display cards for the pictures drawn in from the api in theory
 import { useState } from "react";
-import '../styles/Pic.css';
+// import '../styles/Pic.css';
+import {
+    Card, 
+    Button,
+    Layout,
+} from '@shopify/polaris';
 
 const Pic = ({data}) => {
     const [like, setLike] = useState("like");
     const [love, setLove] = useState("love");
 
     return (
-        <div className="container">
+        <Layout.Section secondary>
+        <Card title={data.title} sectioned>
             <div className="flic">
             <img className="main-img" src={data.url} alt="" />
             <div className="details">
-                <h3 className="title">{data.title}</h3>
+                {/* <h3 className="title">{data.title}</h3> */}
                 <p className="date">{data.date}</p>
-                <div className="heart"><button onClick={(e) => setLike("Liked")}>{like}</button></div>
-                <div className="heart"><button onClick={(e) => setLove("Liked")}>{love}</button></div>
+                <div className="heart"><Button primary onClick={(e) => setLike("Liked")}>{like}</Button></div>
+                <div className="heart"><Button primary onClick={(e) => setLove("Liked")}>{love}</Button></div>
             </div>
         </div>
-    </div>
+    </Card>
+</Layout.Section>
     )
 }
 
